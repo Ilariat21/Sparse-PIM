3fb9cfae39e0e66391a4d54938397571e2e0fa5c
Tairali Assylbekov <assylbekov.21@gmail.com>
2026-04-04 04:09:44 +0900

    updated init.sh script

--- init.sh
+++ init.sh
@@ -1,2 +1,12 @@
 # download the contents of the dramsim3 submodule
 git submodule update --init --recursive
+
+cd dramsim3
+mkdir build
+cd build
+cmake ..
+
+make -j4
+
+cd ../..
+echo dramsim installation is done
